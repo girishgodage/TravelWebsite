9905b5248d3031d2969839afcfd3fe6f315c7aae
Girish Godage <girishgodage@gmail.com>
2018-04-27 03:50:57 +0530

    Commit message

--- meadowlark.js
+++ meadowlark.js
@@ -4,19 +4,26 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 
+
 // set up handlebars view engine
-var handlebars = require('express3-handlebars')
-	.create({ defaultLayout:'main' });
+var handlebars = require('express3-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
-
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
@@ -26,41 +33,38 @@ app.use(function(req, res, next){
 });
 
 app.get('/', function(req, res) {
-	res.render('home'); 
+	res.render('home');
 });
-
-app.get('/about', function(req, res) {
-	 res.render('about', { fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js' }); 
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
-
 app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
 });
-
 app.get('/tours/oregon-coast', function(req, res){
 	res.render('tours/oregon-coast');
 });
-
 app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
 
-// custom 404 page
-app.use(function(req, res){
-        
-         res.status(404);
-         res.render('404');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-// custom 500 page
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
-        console.error(err.stack);
-        
-        res.status(500);
-        res.render('500');
- });
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
 
 app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +    
-  	app.get('port') + '; press Ctrl-C to terminate.' );
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
